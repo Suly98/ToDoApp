de81f9fb453c6a3999d4531672f1e8789cc05360
Suly98 <135811486+Suly98@users.noreply.github.com>
2024-05-25 14:25:07 +0400

    automated the id

--- src/main/java/com/ToDoApp/ToDoApp/ToDo.java
+++ src/main/java/com/ToDoApp/ToDoApp/ToDo.java
@@ -1,10 +1,18 @@
 package com.ToDoApp.ToDoApp;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import lombok.Data;
+import lombok.Generated;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.annotation.Id;
 
 @Data
+
 public class ToDo {
-    Long id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
     String description;
     Boolean isCompleted;
 }
