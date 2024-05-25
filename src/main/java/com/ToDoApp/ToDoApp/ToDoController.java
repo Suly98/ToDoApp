7ee8e3ee20ee78f6b9fd1b4ed9da5f8e9b7aa55a
Suly98 <135811486+Suly98@users.noreply.github.com>
2024-05-25 12:15:39 +0400

    added to a ToDoController

---
+++ src/main/java/com/ToDoApp/ToDoApp/ToDoController.java
@@ -0,0 +1,11 @@
+package com.ToDoApp.ToDoApp;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("todo")
+public class ToDoController {
+
+}
