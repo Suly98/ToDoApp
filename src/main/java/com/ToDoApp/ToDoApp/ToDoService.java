0e3a76855882c60a4478e16f8c78b2256a73f7af
Suly98 <135811486+Suly98@users.noreply.github.com>
2024-05-25 14:20:17 +0400

    added ToDoService

---
+++ src/main/java/com/ToDoApp/ToDoApp/ToDoService.java
@@ -0,0 +1,11 @@
+package com.ToDoApp.ToDoApp;
+
+import jakarta.persistence.Access;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ToDoService {
+
+
+}
