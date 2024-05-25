cb2d62ba7eee663df1e7826a6848c0d86c71bab1
Suly98 <135811486+Suly98@users.noreply.github.com>
2024-05-25 15:27:13 +0400

    added table name

--- src/main/java/com/ToDoApp/ToDoApp/Model/ToDo.java
+++ src/main/java/com/ToDoApp/ToDoApp/Model/ToDo.java
@@ -1,14 +1,17 @@
-package com.ToDoApp.ToDoApp;
+package com.ToDoApp.ToDoApp.Model;
 
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
+import jakarta.persistence.Table;
 import lombok.Data;
 import lombok.Generated;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.annotation.Id;
 
 @Data
-
+@Entity
+@Table(name = "ToDo")
 public class ToDo {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
